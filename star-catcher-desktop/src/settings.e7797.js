256c09c649609fc36eaad02ebd596529e48bafbb
tonyleesos <tonyleesos@hotmail.com>
2020-10-28 23:14:49 +0800

    twelve commit

---
+++ star-catcher-desktop/src/settings.e7797.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/mainmenu.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"7ecb5"}};
