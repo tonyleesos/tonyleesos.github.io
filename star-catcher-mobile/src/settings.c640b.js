c9a46f7971024c0f3db69a25f2ce58f4b7e84a20
tonyleesos <tonyleesos@hotmail.com>
2020-10-29 00:56:29 +0800

    thirdth commit

--- star-catcher-mobile/src/settings.c640b.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/mainmenu.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"7ecb5"}};
